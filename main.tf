79579793abbe01dfecc2f40db2db6986ebf68646
DibyajyotiNayak <ndibyajyotinayak@gmail.com>
2025-10-23 23:27:07 +0530

    check

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 # AWS Provider
 provider "aws" {
-  region-12                  = "us-east-1"
+  region-13                  = "us-east-1"
   access_key              = var.aws_access_key
   secret_key              = var.aws_secret_key
 }
