bed47117f46cc34baf1d8027faf8001f1d49be25
DibyaJyotiNayak <121348451+Dibyaprivate@users.noreply.github.com>
2025-11-17 22:00:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 # AWS Provider
 provider "aws" {
-  region-23                  = "us-west-1"
+  region-23                  = "us-west-3"
   access_key              = var.aws_access_key
   secret_key              = var.aws_secret_key
 }
